df5b0a185bd80adec44181513dbdcdf16a1291fe
Chinmay Garde <chinmaygarde@google.com>
2016-09-12 17:12:06 -0700

    Remove use of mirrors.

--- lib/src/io.dart
+++ lib/src/io.dart
@@ -2,54 +2,25 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@MirrorsUsed(targets: const ['dart.io.HttpClient', 'dart.io.HttpException',
-  'dart.io.File'])
-import 'dart:mirrors';
+import 'dart:io' as io;
 
 /// Whether `dart:io` is supported on this platform.
-bool get supported => _library != null;
-
-/// The `dart:io` library mirror, or `null` if it couldn't be loaded.
-final _library = _getLibrary();
-
-/// The `dart:io` HttpClient class mirror.
-final ClassMirror _httpClient =
-    _library.declarations[const Symbol('HttpClient')];
-
-/// The `dart:io` HttpException class mirror.
-final ClassMirror _httpException =
-    _library.declarations[const Symbol('HttpException')];
-
-/// The `dart:io` File class mirror.
-final ClassMirror _file = _library.declarations[const Symbol('File')];
+bool get supported => true;
 
 /// Asserts that the [name]d `dart:io` feature is supported on this platform.
 ///
 /// If `dart:io` doesn't work on this platform, this throws an
 /// [UnsupportedError].
-void assertSupported(String name) {
-  if (supported) return;
-  throw new UnsupportedError("$name isn't supported on this platform.");
-}
+void assertSupported(String name) {}
 
 /// Creates a new `dart:io` HttpClient instance.
-newHttpClient() => _httpClient.newInstance(const Symbol(''), []).reflectee;
+newHttpClient() => new io.HttpClient();
 
 /// Creates a new `dart:io` File instance with the given [path].
-newFile(String path) => _file.newInstance(const Symbol(''), [path]).reflectee;
+newFile(String path) => new io.File(path);
 
 /// Returns whether [error] is a `dart:io` HttpException.
-bool isHttpException(error) => reflect(error).type.isSubtypeOf(_httpException);
+bool isHttpException(error) => error is io.HttpException;
 
 /// Returns whether [client] is a `dart:io` HttpClient.
-bool isHttpClient(client) => reflect(client).type.isSubtypeOf(_httpClient);
-
-/// Tries to load `dart:io` and returns `null` if it fails.
-LibraryMirror _getLibrary() {
-  try {
-    return currentMirrorSystem().findLibrary(const Symbol('dart.io'));
-  } catch (_) {
-    // TODO(nweiz): narrow the catch clause when issue 18532 is fixed.
-    return null;
-  }
-}
+bool isHttpClient(client) => client is io.HttpClient;
